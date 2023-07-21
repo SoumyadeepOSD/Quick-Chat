228fe8e77ad0b38a6dffb121978ce7a15723975d
SoumyadeepOSD <opencontainer255@gmail.com>
2023-07-21 09:31:46 +0530

    Chat API build successfully in backend server

---
+++ backend/data/data.js
@@ -0,0 +1,15 @@
+const chats = 
+[
+    {
+        id:"1",
+        name:"Soumyadeep",
+        place:"Ocean"
+    },
+    {
+        id:"2",
+        name:"Himanshu",
+        place:"Kolkata"
+    },
+];
+
+module.exports = chats;
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,26 @@
-const app = require('./app');
+const express = require("express");
+const chats = require("./data/data");
+const dotenv = require("dotenv");
 
-const dotenv = require('dotenv');
-const connectDatabase = require('./config/database')
+const app = express();
+dotenv.config();
 
-//config
-dotenv.config({path:'backend/config/config.env'})
+// By default endpoint
+app.get('/',(req, res)=>{
+    res.send("Api is running");
+}); 
 
-//connecting to database
-connectDatabase();
-
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+// 1st endpoint
+app.get('/api/chat', (req, res)=>{
+    res.send(chats);
 });
+
+//2nd endpoint
+app.get('/api/chat/:id', (req, res)=>{
+    const singleChat = chats.find((c) => c.id === req.params.id);
+    res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log("Server started"));
