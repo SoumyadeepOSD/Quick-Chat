eff16e5c557d862e2ebaf2c36777d6f157fd62ea
SoumyadeepOSD <opencontainer255@gmail.com>
2023-07-23 06:47:48 +0530

    Cloudinary functionality added

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
+import {VStack} from "@chakra-ui/layout";
+import {Button} from "@chakra-ui/button"; 
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { useToast } from "@chakra-ui/toast";
 
 const Signup = () => {
     const [name, setName] = useState();
@@ -8,21 +12,63 @@ const Signup = () => {
     const [confirmpassword, setConfirmpassword] = useState();
     const [pic, setPic] = useState();
     const [show, setShow] = useState();
+    const [loading, setLoading] = useState(false);
+    const toast = useToast();
 
     const handleClick = () => {
         setShow(!show);
     }
 
     const postDetails = (pics) => {
-
-    }
-
-    const submitHandler = () => {
-        
-    }
+        setLoading(true);
+        if(pics.type===undefined)
+        {
+            toast({
+                title:"Please select an image",
+                status:"warning",
+                duration:5000,
+                isClosable:true,
+                position:"bottom"
+            });
+            return;
+        }
+        if(pics.type==="image/jpeg" || pics.type==="image/png"){
+            const data = new FormData();
+            data.append("file", pics);
+            data.append("upload_preset","pudkylub");
+            data.append("cloud_name","open-container");
+            fetch("https://api.cloudinary.com/v1_1/open-container/image/upload", {
+                method:"post",
+                body:data,
+            }).then((res)=>res.json())
+            .then((data)=>{
+                setPic(data.url.toString());
+                console.log(data.url.toString());
+                setLoading(false);
+            })
+            .catch((err)=>{
+                console.log(err);
+                setLoading(false);
+            })
+        }else{
+            toast({
+                title:"Sidhumar select an image!",
+                status:"warning",
+                duration:5000,
+                isClosable:true,
+                position:"bottom",
+            });
+            console.log("Please select an image");
+            setLoading(false);
+            return;
+        }
+    } 
+    // Signup button functionality
+    const submitHandler = () => {}
     return (
         <VStack spacing="5px" color="black">
-            <FormControl id='first-name' isRequired>
+            
+             <FormControl id='first-name' isRequired>
                 <FormLabel>Name</FormLabel>
                 <Input
                     placeholder='Enter Your Name' 
@@ -34,9 +80,9 @@ const Signup = () => {
                 <Input
                     placeholder='Enter Your Email' 
                     onChange={(e) => setEmail(e.target.value)} />
-            </FormControl>
+            </FormControl> 
 
-            <FormControl id='password' isRequired>
+             <FormControl id='password' isRequired>
                 <FormLabel>Password</FormLabel>
                 <InputGroup>
                 <Input
@@ -50,7 +96,7 @@ const Signup = () => {
                     </Button>
                 </InputRightElement>
                 </InputGroup>
-            </FormControl>
+            </FormControl> 
 
 
             <FormControl id='confirm-password' isRequired>
@@ -67,25 +113,27 @@ const Signup = () => {
                     </Button>
                 </InputRightElement>
                 </InputGroup>
-            </FormControl>
+            </FormControl> 
 
 
 
-            <FormControl id='pic'>
+            <FormControl id="pic">
                 <FormLabel>Upload your Picture</FormLabel>
                 <Input
-                type='file'
+                type="file"
                 p={1.5}
-                accept='image/*'
+                accept="image/*"
+                // onChange={(e)=>console.log(e.target.files[0])}
                 onChange={(e)=>postDetails(e.target.files[0])}
                 />
             </FormControl>
 
             <Button
-                colorScheme='blue'
+                colorScheme="blue"
                 width="100%"
                 style={{marginTop:15}}
                 onClick={submitHandler}
+                isLoading={loading}
             >
                 Sign Up
             </Button>
@@ -93,4 +141,4 @@ const Signup = () => {
     )
 }
 
-export default Signup
+export default Signup;
